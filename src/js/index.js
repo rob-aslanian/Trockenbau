eb2d47d0f603ffb785ad9b8962df122ecb565535
Nodar32 <nodarrash@gmail.com>
2019-01-04 19:01:22 +0400

    /////////

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,74 @@
-import $ from 'jquery' // Jquery 
+import $ from "jquery"; // Jquery
+import "slick-carousel";
 
-$(function(){
+$(function() {
+  /** Smooth Scrollin */
+  // $('YOUR CLASS').click(function(e){
 
+  //     e.preventDefault();
+  //     $('html  , body').animate({
+  //         scrollTop: $(this.hash).length === 0 ?
+  //                     $(this).offset().top :
+  //                     $(this.hash).offset().top
+  //     }, 1000);
+  // });
 
-    /** Smooth Scrollin */
-    // $('YOUR CLASS').click(function(e){
+  /** Menu Collapse */
 
-    //     e.preventDefault();
-    //     $('html  , body').animate({
-    //         scrollTop: $(this.hash).length === 0 ? 
-    //                     $(this).offset().top : 
-    //                     $(this.hash).offset().top
-    //     }, 1000);
-    // });
-    
-    /** Menu Collapse */
-    $('.collapse-btn').click(function(e){
+  $(".collapse-btn").click(function(e) {
+    const collapse = $(".collapse");
 
-        const collapse = $('.collapse');
-
-        if($(window).resize(function(e) { 
-            if($(window).width() > 768){
-                collapse.fadeIn(10);
-            }
-            else{
-                collapse.fadeOut(0);
-            }
-        }))
-
-        if(!e.target.classList.contains('collapsed')){
-            e.target.classList.add('collapsed');
-            collapse.slideDown(350);
+    if (
+      $(window).resize(function(e) {
+        if ($(window).width() > 768) {
+          collapse.fadeIn(10);
+        } else {
+          collapse.fadeOut(0);
         }
-        else{
-            e.target.classList.remove('collapsed');
-            collapse.slideUp(350);
-          
-        } 
-        
-    });
+      })
+    )
+      if (!e.target.classList.contains("collapsed")) {
+        e.target.classList.add("collapsed");
+        collapse.slideDown(350);
+      } else {
+        e.target.classList.remove("collapsed");
+        collapse.slideUp(350);
+      }
+  });
 
-    // Your code here....
-
+  $("#slider-area").slick({
+    // dots: false,
+    // centerMode: true,
+    // infinite: false,
+    // speed: 300,
+    // slidesToShow: 1,
+    // slidesToScroll: 1
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //       infinite: true,
+    //       dots: true
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1
+    //     }
+    //   }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  });
 });
